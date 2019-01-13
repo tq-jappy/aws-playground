61412e814c008f4cc7e06749de157452a3800289
Takumi Endo <tq-ed@v7.com>
2019-01-14 02:00:29 +0900

    fix syntax error

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,15 +1,15 @@
-import { DynamoDB } = require('aws-sdk');
+import { DynamoDB } from 'aws-sdk';
 
 exports.handler = async (event: any) => {
   console.log(JSON.stringify(event, undefined, 2));
 
-  const client = new DynamoDB.Document();
+  const client = new DynamoDB.DocumentClient();
 
   for (const record of event.Records) {
     const body = record.body ? JSON.parse(record.body) : {};
 
     await client.put({
-      TableName: process.env.TABLE_NAME,
+      TableName: process.env.TABLE_NAME || '',
       Item: {
         id: record.messageId,
         text: body.text,
