6ce62c1e60973a83f3ae069cf01ee0be08b02a5e
Takumi Endo <tq-ed@v7.com>
2019-08-20 01:37:57 +0900

    remove old bin/cdk.js

--- bin/cdk.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cdk = require("@aws-cdk/cdk");
-const cdk_stack_1 = require("../lib/cdk-stack");
-const app = new cdk.App();
-new cdk_stack_1.CdkStack(app, 'CdkStack');
-app.run();
